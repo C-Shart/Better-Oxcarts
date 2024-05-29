3f2e4603741af50eae35f8979f9cacc1d5fef577
Aaron <C-Shart@users.noreply.github.com>
2024-05-28 22:04:12 -0700

    Refactor, +cart parts invincibility

--- Todos.md
+++ Todos.md
@@ -1,5 +1,4 @@
 ## NOW
-+ Set cart parts invincible too (app.Gm80_042.PartsList[], app.Sm80_042_Parts.CompHit.IsInvincible)
 + BUG: Game loads same oxcart & guards
     + Find a way to refresh the cart. Finding this should enable me to also allow cart configuration
 + BUG: Wandering too far from cart makes cart fall apart & disappear
@@ -7,7 +6,8 @@
 
 ## SOON
 + Faster ox
-+ Guard buffs (advanced abilities, buffed stats)
++ Guard buffs (advanced abilities, buffed stats, better equipment)
++ Ox buffs (buffed stats)
 + User configurability
  
 ## LATER
@@ -18,3 +18,78 @@
  
 ### DONE
 + "set invincible" logic to its own function
++ Set cart parts invincible too (app.Gm80_042.PartsList[], app.Sm80_042_Parts.CompHit.IsInvincible)
+
+
+
+
+app.OxcartManager.getSchedule()
+                  getOxcartStatusfromAssign()
+                  getStatus(oxcart id)
+
+app.Oxcartstatus.setDriver
+                .addGuard()
+
+app.Character:
+    app.CharacterInput                  <Input>k__BackingField
+    app.WeaponandItemHolder             <WeaponAndItemHolder>k__BackingField
+    app.StaminaManager                  <StaminaManager>k__BackingField
+    app.DangerController                DangerCtrl
+    app.CharacterAIStateController      CharaAIStateCtrl
+    app.BattleStateController           BattleStateCtrl
+    app.SetTempStatusTrack
+    app.Human                           <Human>k__BackingField
+
+app.Human:
+    app.HumanJobChanger
+        :setup(app.Human)
+    app.JobContext
+        ["CurrentJob"]
+        ["MaxEquipList"]
+        ["EquipedArmors"]
+    app.JobMagicUserContext
+    app.HumanStatuscontext
+    app.HumanSkillContext
+    app.AbilityContext
+    .getJobChanger()
+    .getSpellStockCtrl()
+    .get_AbilityContext()
+
+app.NPCManager:
+    .get_NPCList()
+    .getNPCConfig()
+    .getNPCCharacterData()
+    .setupCharacterData()
+
+app.Ch299003:
+    ["RunRate"]
+    ["MotSpeedMax"]
+    ["MotSpeedMin"]
+
+app.OxcartUtil
+    getOxcartStatus(app.CharacterID)
+    getOxcartStatus_AssignCharacterID(app.ChracterID)
+    getOxcartScheduleData
+    getOxcartStatus_Entity
+
+app.OxcartAI
+app.OxcartAI.DriverAction
+
+
+app.Gm80_042
+    .get_BoardingRouteNum()
+    .setupObjects()
+
+
+
+EXXXcellent — 05/21/2024 12:17 PM
+Do what I do as a more global approach so that you can ignore stamina recovery/consumption with any ability, set a flag and hook the StaminaController like so:
+
+sdk.hook(
+        sdk.find_type_definition("app.HumanStaminaController"):get_method("recoverStamina"),
+        function(args)
+            -- do checks to make sure it's the player's stamina controller here
+            if not no_recover_flag then return end
+            return sdk.PreHookResult.SKIP_ORIGINAL
+        end
+)
--- reframework/autorun/BetterOxcarts.lua
+++ reframework/autorun/BetterOxcarts.lua
@@ -2,8 +2,18 @@ local modname="[BetterOxcarts]"
 
 local _NPCManager = sdk.get_managed_singleton("app.NPCManager")
 local top_char_ids = {}
-local top_cids_index = nil
+--local top_parts_list = {}
 local this_ox_id = 0
+
+-- Hotkeys for testing
+--[[
+local hotkey_config = {}
+hotkey_config.Hotkeys = {
+    ["OxcartStatus"] = "I"
+}
+hotkeys.setup_hotkeys(hotkey_config.Hotkeys)
+]]
+
 
 -- 05/14 I don't think I need this anymore actually?
 -- 05/16 Keeping it until I'm positive I don't need it for anything anymore
@@ -11,94 +21,51 @@ local this_ox_id = 0
 local function generate_char_ids()
     local character_ids = sdk.find_type_definition("app.CharacterID")
     if not character_ids then return {} end
-    
     local fields = character_ids:get_fields()
     local enum = {}
-    
     for i, field in ipairs(fields) do
         local name = field:get_name()
         local raw_value = field:get_data()
-        
         if raw_value then
             enum[raw_value] = name
-        end    
-    end    
+        end
+    end
     return enum
-end    
-
--- Skips execution of any hooked method.
-local function skip_execution(args)
-    return sdk.PreHookResult.SKIP_ORIGINAL
 end
-
--- Hooks onDamageHit and onCalcDamageEnd for the oxcart and skips them.
-sdk.hook(
-    sdk.find_type_definition("app.Gm80_042"):get_method("onDamageHit"),
-    skip_execution
-)
-sdk.hook(
-    sdk.find_type_definition("app.Gm80_042"):get_method("onCalcDamageEnd"),
-    skip_execution
-)
-
--- Hooks executeBreak and skips it so the cart doesn't automatically break in certain circumstances.
-sdk.hook(
-    sdk.find_type_definition("app.Gm80_042"):get_method("executeBreak"),
-    skip_execution
-)
-sdk.hook(
-    sdk.find_type_definition("app.Sm80_042_Parts"):get_method("executeBreak"),
-    skip_execution
-)
-
 local char_ids = generate_char_ids()
+
 -- Checks if table tbl has a given value val
 local function has_character_id(tbl, val)
     for k,v in ipairs(tbl) do
         if v == val then
             top_cids_index = k
             return true 
-        end        
-    end        
+        end
+    end
     return false
 end
 
-
--- Hooks registNPC and checks if the character is in top_char_ids, then sets invuln if so.
-sdk.hook(
-    _NPCManager:get_type_definition():get_method("registNPC"),
-    function(args)
-        if #top_char_ids > 0 then
-            if has_character_id(top_char_ids, sdk.to_managed_object(args[3])["CharacterID"]) then
-                local char = sdk.to_managed_object(args[3])
-                local reg_chara_id = char["CharacterID"]
-                local char_hitctrl = char["<Hit>k__BackingField"]
-                local is_char_invuln = char_hitctrl["<IsInvincible>k__BackingField"]
-                --print(" --inv : " .. tostring(is_char_invuln))
-                
-                if is_char_invuln ~= true then
-                    char_hitctrl:set_field("<IsInvincible>k__BackingField", true)
-                    --print(" --inv : " .. tostring(char_hitctrl["<IsInvincible>k__BackingField"]))
-                end        
-                table.remove(top_char_ids, top_cids_index)
-            end        
-        end        
-    end        
-)        
+-- Skips execution of any hooked method.
+local function skip_execution(args)
+    return sdk.PreHookResult.SKIP_ORIGINAL
+end
 
 -- Makes things invincible
 local function make_invincible(obj)
+    print("Character   : " .. tostring(obj["CharacterID"]))
     local obj_hit_ctrl = obj["<Hit>k__BackingField"]
     if obj_hit_ctrl then
         local is_invincible = obj_hit_ctrl["<IsInvincible>k__BackingField"]
+        print(" - IsInvinc : " .. tostring(is_invincible))
         if is_invincible ~= true then
             obj_hit_ctrl:set_field("<IsInvincible>k__BackingField", true)
+            print(" - IsInvinc : " .. tostring(obj_hit_ctrl["<IsInvincible>k__BackingField"]))
         end
     end
 end
 
--- Does what it says on the tin
-local function make_ox_and_cart_invincible(oxobj)
+-- Makes ox invincible
+local function make_ox_invincible(oxobj)
     this_ox_id = 0
     local ox_object = sdk.to_managed_object(oxobj)
     if ox_object then
@@ -108,7 +75,7 @@ local function make_ox_and_cart_invincible(oxobj)
     end
 end
 
--- Gets driver & guards & does stuff to them >:3
+ -- Gets driver & guards & does stuff to them >:3
 local function make_driver_and_guards_invincible(oid)
     local oxcart_status = _NPCManager["OxcartManager"]:getStatus(oid)
     local driver_field = oxcart_status["DriverID"]
@@ -139,8 +106,107 @@ end
 sdk.hook(
     sdk.find_type_definition("app.Ch299003"):get_method("connectOxcart"),
     function(args)
-        make_ox_and_cart_invincible(args[2])
+        print("")
+        print("")
+        print("====== BEGINNING INVINC CHECK/SET =======")
+        make_ox_invincible(args[2])
+        --make_cart_invincible(args[3])
         make_driver_and_guards_invincible(this_ox_id)
+        -- top_cart_obj = sdk.to_managed_object(args[3])
     end,
     nil
 )
+
+-- Hooks methods to avoid the cart and its parts taking damage
+sdk.hook(
+    sdk.find_type_definition("app.Gm80_042"):get_method("onDamageHit"),
+    skip_execution
+)
+sdk.hook(
+    sdk.find_type_definition("app.Gm80_042"):get_method("onCalcDamageEnd"),
+    skip_execution
+)
+sdk.hook(
+    sdk.find_type_definition("app.Gm80_042"):get_method("executeBreak"),
+    skip_execution
+)
+sdk.hook(
+    sdk.find_type_definition("app.Sm80_042_Parts"):get_method("executeBreak"),
+    skip_execution
+)
+sdk.hook(
+    sdk.find_type_definition("app.Sm80_042_Parts"):get_method("callbackDamageHit"),
+    skip_execution
+)
+
+-- Takes the cart parts and makes them invincible
+ sdk.hook(
+    sdk.find_type_definition("app.Sm80_042_Parts"):get_method("start"),
+    function(args)
+        local this_part = sdk.to_managed_object(args[2])
+        print(" --CARTPART : " .. tostring(this_part))
+        local part_hitctrl = this_part["<CompHitCtrl>k__BackingField"]
+        if part_hitctrl then
+            local is_invincible = part_hitctrl["<IsInvincible>k__BackingField"]
+            print(" - IsInvinc : " .. tostring(is_invincible))
+            if is_invincible ~= true then
+                part_hitctrl:set_field("<IsInvincible>k__BackingField", true)
+                print(" - IsInvinc : " .. tostring(part_hitctrl["<IsInvincible>k__BackingField"]))
+            end
+        end
+    end
+)
+
+-- Hooks registNPC and checks if the character is in top_char_ids, then sets invuln if so.
+sdk.hook(
+    _NPCManager:get_type_definition():get_method("registNPC"),
+    function(args)
+        if #top_char_ids > 0 then
+            npc_object = sdk.to_managed_object(args[3])
+            if has_character_id(top_char_ids, npc_object["CharacterID"]) then
+                make_invincible(npc_object)
+                table.remove(top_char_ids, top_cids_index)
+            end
+        end
+    end
+)
+
+local function get_oxcart_status(oid)
+    local status = _NPCManager["OxcartManager"]:getStatus(oid)
+
+    ----- CHANGE
+    local driver_field = oxcart_status["DriverID"]
+    local driver_id = driver_field.CharaID
+    local driver = _NPCManager:getCharacter(driver_id)
+    if driver then
+        make_invincible(driver)
+    else
+        table.insert(top_char_ids, driver_id)
+    end
+
+    local guards_list = oxcart_status["_Guards"]
+    local guards_length = guards_list._items:get_size()
+    for i=0, guards_length do
+        if guards_list[i] then
+            local guard = guards_list[i].CharaID
+            if not _NPCManager:getCharacter(guard) then
+                table.insert(top_char_ids, guard)
+            else
+                local guard_char = _NPCManager:getCharacter(guard)
+                make_invincible(guard_char)
+            end
+        end
+    end
+
+end
+
+--[[
+re.on_application_entry("LateUpdateBehavior", function()
+    if hotkeys.check_hotkey("OxcartStatus", true, false) then
+        print("")
+        print("==== OXCART STATUS ====")
+        get_oxcart_status(this_ox_id)
+    end
+end
+) 
+]]
